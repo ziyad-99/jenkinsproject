d47e323b842e0fc7a224ee5517c3bb5037513ab7
ziyad-99 <falconbird39@gmail.com>
2023-01-04 19:48:59 +0100

    persons java file is updated by ziyad

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -47,6 +47,13 @@ public class PersonService {
 		p.setLastName("success");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("6");
+		p.setAge(25);
+		p.setFirstName("med ziyad hafri");
+		p.setLastName("hafri");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
